97e32ce63a76082219215afa11ee7ad7f2569e54
dashkaix <117798237+dashkaix@users.noreply.github.com>
2024-11-06 23:15:09 +0530

    added comments in outputs.tf

--- Day-3/outputs.tf
+++ Day-3/outputs.tf
@@ -1,3 +1,10 @@
 output "public_ip" {
   value = module.ec2_instance.public_ip_address
 }
+
+#To all who are searching for the output after applying: create a output variable file in the root directory and refer to the variable
+#using module.module_name.output_variable_name_in_module..... 
+#like
+#output "public_ip" {
+#  value = module.ec2_instance.your_output_variable_in_module
+#} 
